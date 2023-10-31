4578e54173549fc6dc0ba48f98f5697d090ad9bf
duongdd141100 <duongdd141100@gmail.com>
2023-10-31 08:31:07 +0700

    Fix request mapping for order controller

--- OOF Backend/src/main/java/com/example/oof_badminton/controller/OrderController.java
+++ OOF Backend/src/main/java/com/example/oof_badminton/controller/OrderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/order")
+@RequestMapping("/api/orders")
 @Slf4j
 public class OrderController {
 
