e8cdbb449aa5ade862e9562a8ba20f87dc924742
duongdd141100 <duongdd141100@gmail.com>
2023-11-04 23:12:04 +0700

    Upate get categories

--- OOF Backend/src/main/java/com/example/oof_badminton/entity/Category.java
+++ OOF Backend/src/main/java/com/example/oof_badminton/entity/Category.java
@@ -13,6 +13,9 @@ public class Category extends BaseEntity {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "path")
+    private String path;
+
     @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
     private List<Product> products;
 
