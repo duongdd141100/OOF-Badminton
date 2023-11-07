6c03d0c63ed989608785f6ab8b2b06dab886f2b1
hungpv <phanhung2702@gmail.com>
2023-11-08 01:19:23 +0700

    [Update] product craft

--- OOF-Frontend/src/views/product/index.js
+++ OOF-Frontend/src/views/product/index.js
@@ -3,10 +3,43 @@ import { getRequest } from '../../providers/request'
 import './index.css'
 import { useLocation } from "react-router-dom"
 import { BASE_URL } from '../../constant'
+import { Radio, Space } from 'antd'
+import { useNavigate } from 'react-router'
+
+const orderOptions = [
+  { label: 'Increase', value: 'increase' },
+  { label: 'Degrease', value: 'degrease' },
+]
 
 export const Product = (props) => {
   const [products, setProducts] = useState([])
+  const [groupOfFour, setGroupOfFour] = useState([])
+  const navigate = useNavigate()
   const location = useLocation();
+
+  function chunkArray(arr, chunkSize) {
+    const chunkedArray = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      chunkedArray.push(arr.slice(i, i + chunkSize));
+    }
+    return chunkedArray;
+  }
+
+  function onFilterChange(e) {
+    const nextProducts = JSON.parse(JSON.stringify(products))
+    nextProducts.sort((current, next) => {
+      if (e.target.value === 'increase') {
+        return current.price - next.price
+      }
+      return next.price - current.price
+    })
+    setProducts(nextProducts)
+  }
+
+  function handleProductClick(id) {
+    navigate(`/productDetail/${id}`, {state: {id}})
+  }
+
   useEffect(() => {
     const currentCategoryName = location?.state?.category
     getRequest('categories').then(data => {
@@ -25,32 +58,40 @@ export const Product = (props) => {
       }
     })
   }, [])
+
+  useEffect(() => {
+    const chunkSize = 4
+    const result = chunkArray(JSON.parse(JSON.stringify(products)), chunkSize)
+    setGroupOfFour(result)
+  }, [products])
+
   return (
     <div className='products'>
+      <div className="filter">
+        <Radio.Group onChange={onFilterChange}>
+          <Space direction="vertical">
+            {orderOptions.map((item) => {
+              return <Radio value={item.value}>{item.label}</Radio>
+            })}
+          </Space>
+        </Radio.Group>
+      </div>
       <div className='products-container'>
-        <div className='products-column'>
-          {products.length && products.map((item, index) => {
-            if (index < products.length / 2) {
-              return (
-              <div className='product-card'>
-                <img className='product-image' src={`${BASE_URL}${item.imagePath}`} width='100%' height='80%' />
-                <div className='product-name' style={{ display: 'flex', justifyContent: 'center' }}>{item.productName}</div>
-                <div className='product-price' style={{ display: 'flex', justifyContent: 'center', color: 'rgb(214, 81, 123)' }}>{item.price} đ</div>
+          {groupOfFour.length && groupOfFour.map((item) => {
+            return(
+              <div className='products-row'>
+                {item.map(element => {
+                    return (
+                    <div className='product-card' onClick={() => {handleProductClick(element.id)}}>
+                      <img className='product-image' src={`${BASE_URL}${element.imagePath}`} width='100%' height='80%' />
+                      <div className='product-name' style={{ display: 'flex', justifyContent: 'center' }}>{element.productName}</div>
+                      <div className='product-price' style={{ display: 'flex', justifyContent: 'center', color: 'rgb(214, 81, 123)' }}>{element.price} đ</div>
+                    </div>
+                  )
+                })}
               </div>
             )
-          }
           })}
-        </div>
-        <div className='products-column'>
-          {products.length && products.map((item, index) => {
-            if (index >= products.length / 2) return (<div className='product-card'>
-                <img className='product-image' src={`${BASE_URL}${item.imagePath}`} width='100%' height='80%' />
-                <div className='product-name' style={{ display: 'flex', justifyContent: 'center' }}>{item.productName}</div>
-                <div className='product-price' style={{ display: 'flex', justifyContent: 'center', color: 'rgb(214, 81, 123)' }}>{item.price} đ</div>
-              </div>
-              )
-          })}
-        </div>
       </div>
     </div>
   )
